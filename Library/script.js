70ff14b9afe82627e76cf7da8fbdb3a9d29c0e0b
Verthon <krzysiekksordyl@gmail.com>
2018-05-05 22:03:54 +0200

    library excersise init

---
+++ Library/script.js
@@ -0,0 +1,12 @@
+const toggleModal = () =>{
+  const modal = document.querySelector('#modal')
+  console.log(modal.className);
+  
+}
+
+const btn = document.querySelector('#add');
+btn.addEventListener('click', toggleModal);
+
+const addBook = () =>{
+  
+}
